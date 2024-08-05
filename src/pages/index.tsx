8971cd7e0e21bfb3773a1efc2eb9edcf03e43163
GuevaraMarius <irabizimguevara@gmail.com>
2024-08-05 22:23:06 +0200

    Setup husky

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,14 @@
-// src/pages/index.tsx
-import React from 'react';
-
-import { formatDate } from '../utils/functions/date';
-import BlogPost from '@/components/data/blogPost';
-
+import React from "react";
+import { formatDate } from "@/utils/functions/date";
+import BlogPost from "@/components/BlogPost";
 
 const Home: React.FC = () => {
   const blog = {
-    title: 'Embracing the Foundations of Development',
-    description: 'A comprehensive guide to understanding the basics of web development.',
-    date: '2024-08-02T00:00:00Z', 
-    link: 'https://dev.to/irabiziguevara/embracing-the-foundations-of-development-7lf'
+    title: "Embracing the Foundations of Development",
+    description:
+      "A comprehensive guide to understanding the basics of web development.",
+    date: "2024-08-02T00:00:00Z",
+    link: "https://dev.to/irabiziguevara/embracing-the-foundations-of-development-7lf",
   };
 
   return (
